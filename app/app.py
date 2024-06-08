4d36610402598c1997c5125013ee8ee769565ef6
DaryaTrifonova <113592931+DaryaTrifonova@users.noreply.github.com>
2024-06-08 14:31:04 +0300

    Adding actions with books
    Добавлены редактирование и удаление книги. Реализован макрос для редактирования/создания и модальное окно для удаления

--- app/app.py
+++ app/app.py
@@ -4,6 +4,7 @@ from flask_migrate import Migrate
 from models import db, Book, Genre, Image
 from flask_login import login_required
 from tools import ImageSaver, extract_params
+import os
 
 
 app = Flask(__name__)
@@ -23,7 +24,7 @@ app.register_blueprint(auth_bp)
 @app.route('/')
 def index():
     page = request.args.get('page', 1, type=int)
-    books = Book.query.order_by(Book.id.desc())
+    books = Book.query.order_by(Book.year_release.desc())
     pagination = books.paginate(page=page, per_page=app.config['PER_PAGE'])
     genres = Genre.query.all()
     books = pagination.items
@@ -72,7 +73,7 @@ def new():
                 db.session.add(book)
                 db.session.commit()
                 flash('Книга успешно добавлена', 'success')
-                return redirect(url_for('new'))
+                return redirect(url_for('show', book_id = book.id))
             except Exception as e:
                 db.session.rollback()
                 flash(f'При сохранении данных возникла ошибка: {e}', 'danger')
@@ -81,6 +82,82 @@ def new():
     
     genres = Genre.query.all()
     return render_template('books/new_edit.html',
-                           action_category='create',
+                           action='create',
                            genres=genres,
                            book={})
+
+@app.route('/edit/<int:book_id>', methods=["POST", "GET"])
+@login_required
+@check_rights('edit')
+def edit(book_id):
+    if request.method == "POST":
+        params = extract_params(BOOKS_PARAMS)
+        params['year_release'] = int(params['year_release'])
+        params['pages_volume'] = int(params['pages_volume'])
+        genres = request.form.getlist('genres')
+        genres_list = []
+        book = Book.query.get(book_id)
+        book.name = params['name']
+        book.author = params['author']
+        book.publisher = params['publisher']
+        book.year_release = params['year_release']
+        book.pages_volume = params['pages_volume']
+        book.short_desc = params['short_desc']
+
+        for i in genres:
+            genre = Genre.query.filter_by(id=i).first()
+            genres_list.append(genre)
+        book.genres = genres_list
+
+        # book = Book(**params)
+        # book.prepare_to_save() 
+        # book.genres = genres_list
+        try:
+            db.session.add(book)
+            db.session.commit()
+            flash('Книга успешно отредактирована', 'success')
+            return redirect(url_for('show', book_id = book.id))
+        except Exception as e:
+            db.session.rollback()
+            flash(f'При сохранении данных возникла ошибка: {e}', 'danger')
+    
+    book = Book.query.get(book_id)
+    genres = Genre.query.all()
+    return render_template('books/new_edit.html',
+                           action='edit',
+                           genres=genres,
+                           book=book)
+
+@app.route('/delete/<int:book_id>', methods=["POST"])
+@login_required
+@check_rights('delete')
+def delete(book_id):
+
+    book = Book.query.get(book_id)
+    books_with_img = len(Book.query.filter_by(image_id=book.image.id).all())
+    try:
+        db.session.delete(book)
+
+        if books_with_img == 1:
+                image = Image.query.get(book.image.id)
+                delete_path = os.path.join(
+                    app.config['UPLOAD_FOLDER'],
+                    image.file_name)
+                db.session.delete(image)
+                os.remove(delete_path)
+
+        db.session.commit()
+        flash('Удаление книги прошло успешно', 'success')
+    except:
+        db.session.rollback()
+        flash('Во время удаления книги произошла ошибка', 'danger')
+
+    return redirect(url_for('index'))
+
+
+@app.route('/<int:book_id>')
+def show(book_id):
+    book = Book.query.get(book_id)
+    book.prepare_to_html()
+    return render_template('books/show.html',
+                           book=book)
